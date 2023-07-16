26a105a53f0583927e69ff0ece23a458060d22a9
Pierre Massey <pierremassey1@gmail.com>
2023-07-16 02:20:03 +0200

    [feature] reduce image embedding size.

--- drl_nav/network/bodies.py
+++ drl_nav/network/bodies.py
@@ -11,18 +11,26 @@ class ConvBody(nn.Module):
     def __init__(self, in_channels=3, input_size=84, **kwargs):
         super(ConvBody, self).__init__()
 
+        # 84*84*3
         self.conv1 = nn.Conv2d(in_channels, 4, kernel_size=5)
         self.mp1 = nn.MaxPool2d(kernel_size=2, stride=2)
         
-        self.conv2 = nn.Conv2d(4, 8, kernel_size=3)
+        # 40*40*4
+        self.conv2 = nn.Conv2d(4, 6, kernel_size=5)
         self.mp2 = nn.MaxPool2d(kernel_size=2, stride=2)
 
+        # 18*18*16
+        self.conv3 = nn.Conv2d(6, 8, kernel_size=3)
+        self.mp3 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # 8*8*8
         self.output_size = compute_output_size(self, input_size, in_channels)
 
     def forward(self, x):
 
         x = self.mp1(F.relu(self.conv1(x)))
         x = self.mp2(F.relu(self.conv2(x)))
+        x = self.mp3(F.relu(self.conv3(x)))
 
         output = x.view(x.size(0), -1)
 
