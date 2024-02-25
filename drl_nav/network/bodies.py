3ab3aa4a8fb1e9a49289d71d306b65bdbc69da26
Pierre Massey <pierremassey1@gmail.com>
2024-02-25 23:31:03 +0100

    [feature] conv net without pooling.

--- drl_nav/network/bodies.py
+++ drl_nav/network/bodies.py
@@ -21,6 +21,49 @@ class ConvBody(nn.Module):
         """
         super(ConvBody, self).__init__()
         # 84*84*3
+        self.conv1 = nn.Conv2d(in_channels, out_channels=4, kernel_size=5, stride=2)
+        #self.mp1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        
+        # 40*40*4
+        self.conv2 = nn.Conv2d(4, 6, kernel_size=5, stride=2)
+        #self.mp2 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # 18*18*6
+        self.conv3 = nn.Conv2d(6, 8, kernel_size=3, stride=2)
+        #self.mp3 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # 8*8*8
+        self.output_size = compute_output_size(self, input_size, in_channels)
+        device = kwargs.get('device', 'cpu')
+        print(f'Body to {device}')
+        self.to(device)
+
+    def forward(self, x):
+
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+
+        output = x.view(x.size(0), -1)
+
+        return output
+    
+class ConvPoolBody(nn.Module):
+    """
+    Convolutional network that convert pixel state into features.
+    """
+    def __init__(
+            self,
+            in_channels: int = 3,
+            input_size: int = 84,
+            **kwargs: Dict):
+        """
+        Args:
+            in_channels: number of channels of the image. Defaults to 3.
+            input_size: size of the image (given that width = height). Defaults to 84.
+        """
+        super(ConvPoolBody, self).__init__()
+        # 84*84*3
         self.conv1 = nn.Conv2d(in_channels, 4, kernel_size=5)
         self.mp1 = nn.MaxPool2d(kernel_size=2, stride=2)
         
